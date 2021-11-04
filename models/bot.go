7e63802073e299c16b148037ddbd9b3b9d890ff4
ZJY <764763903@qq.com>
2021-11-04 14:39:55 +0800

    fix 大赢家全自动脚本

--- models/bot.go
+++ models/bot.go
@@ -416,8 +416,11 @@ func startdyj(ine string, red string) (num int) {
 		if strings.Contains(data, "恭喜帮好友助力成功") {
 			i++
 		} else {
+			i++
 			logs.Info(data)
 			logs.Info("火爆了")
+		}
+		if i == 10 {
 			return
 		}
 	}
