06532ecc92d77259e27c7c0cb777fa9b50f284de
ZJY <764763903@qq.com>
2021-11-15 00:48:41 +0800

    add 调试认证

--- models/auth.go
+++ models/auth.go
@@ -1,1 +1,29 @@
 package models
+
+import (
+	"crypto/md5"
+	"encoding/hex"
+	"fmt"
+	"net"
+)
+
+func GetLocalMac() (mac string) {
+	// 获取本机的MAC地址
+	interfaces, err := net.Interfaces()
+	if err != nil {
+		panic("Poor soul, here is what you got: " + err.Error())
+	}
+	for _, inter := range interfaces {
+		fmt.Println(inter.Name)
+		mac := inter.HardwareAddr //获取本机MAC地址
+		fmt.Println("MAC = ", mac)
+	}
+	return GetAuthKey(mac)
+}
+
+func GetAuthKey(token string) (key string) {
+	md5Ctx := md5.New()
+	md5Ctx.Write([]byte(token))
+	data := md5Ctx.Sum(nil)
+	return hex.EncodeToString(data)
+}
