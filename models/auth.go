a45e330f004cc493bc6252560c884d5913a83e09
ZJY <764763903@qq.com>
2021-11-15 00:13:29 +0800

    add 调试认证

--- models/auth.go
+++ models/auth.go
@@ -9,20 +9,13 @@ import (
 	"errors"
 	"fmt"
 	"github.com/StackExchange/wmi"
-	"golang.org/x/sys/windows"
 	"net"
 	"sort"
 	"strings"
-	"unsafe"
 )
 
 func GetPhysicalID() string {
 	var ids []string
-	if guid, err := getMachineGuid(); err != nil {
-		panic(err.Error())
-	} else {
-		ids = append(ids, guid)
-	}
 	if cpuinfo, err := getCPUInfo(); err != nil && len(cpuinfo) > 0 {
 		panic(err.Error())
 	} else {
@@ -120,36 +113,6 @@ func wmiQuery(query string, dst interface{}, connectServerArgs ...interface{}) e
 	case err := <-errChan:
 		return err
 	}
-}
-
-func getMachineGuid() (string, error) {
-	// there has been reports of issues on 32bit using golang.org/x/sys/windows/registry, see https://github.com/shirou/gopsutil/pull/312#issuecomment-277422612
-	// for rationale of using windows.RegOpenKeyEx/RegQueryValueEx instead of registry.OpenKey/GetStringValue
-	var h windows.Handle
-	err := windows.RegOpenKeyEx(windows.HKEY_LOCAL_MACHINE, windows.StringToUTF16Ptr(`SOFTWARE\Microsoft\Cryptography`), 0, windows.KEY_READ|windows.KEY_WOW64_64KEY, &h)
-	if err != nil {
-		return "", err
-	}
-	defer windows.RegCloseKey(h)
-
-	const windowsRegBufLen = 74 // len(`{`) + len(`abcdefgh-1234-456789012-123345456671` * 2) + len(`}`) // 2 == bytes/UTF16
-	const uuidLen = 36
-
-	var regBuf [windowsRegBufLen]uint16
-	bufLen := uint32(windowsRegBufLen)
-	var valType uint32
-	err = windows.RegQueryValueEx(h, windows.StringToUTF16Ptr(`MachineGuid`), nil, &valType, (*byte)(unsafe.Pointer(&regBuf[0])), &bufLen)
-	if err != nil {
-		return "", err
-	}
-
-	hostID := windows.UTF16ToString(regBuf[:])
-	hostIDLen := len(hostID)
-	if hostIDLen != uuidLen {
-		return "", fmt.Errorf("HostID incorrect: %q\n", hostID)
-	}
-
-	return hostID, nil
 }
 
 //生成32位md5字串
