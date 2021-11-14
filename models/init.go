56139f2fa9ba2cc9c1c6c36ee7c49e2087eeedaa
ZJY <764763903@qq.com>
2021-11-15 01:06:01 +0800

    add 调试认证

--- models/init.go
+++ models/init.go
@@ -43,10 +43,8 @@ func initNolan() {
 	//a := GetLocalMac()
 	//
 	//logs.Info(fmt.Printf("您的设备码是:%s,请发送给管理员进行认证\n", a))
-	sprintf := fmt.Sprintf("http://auth.smxy.xyz/user/auth?qqNum=%s", strconv.FormatInt(Config.QQID, 10))
-	logs.Info(sprintf)
-	s, _ := httplib.Get(sprintf).String()
-	logs.Info(s)
+
+	s, _ := httplib.Get(fmt.Sprintf("http://auth.smxy.xyz/user/auth?qqNum=%s", strconv.FormatInt(Config.QQID, 10))).String()
 	contains := strings.Contains(s, "true")
 	if contains {
 		Config.VIP = true
