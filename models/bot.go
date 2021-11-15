bbce65bb48cc060ff44df278a75f9b656e4c59a3
ZJY <764763903@qq.com>
2021-11-15 17:38:08 +0800

    测试临时对话

--- models/bot.go
+++ models/bot.go
@@ -265,6 +265,28 @@ var handleMessage = func(msgs ...interface{}) interface{} {
 				}
 			}
 		}
+		{
+			//发财挖宝
+			if Config.VIP == true {
+				//dyj
+				inviterId := regexp.MustCompile(`inviterId=(\S+)(&|&amp;)inviterCode`).FindStringSubmatch(msg)
+				inviterCode := regexp.MustCompile(`inviterCode=(\S+)(&|&amp;)utm`).FindStringSubmatch(msg)
+				logs.Info(inviterId)
+				logs.Info(inviterCode)
+				if len(inviterId) > 0 && len(inviterCode) > 0 {
+					if !sender.IsAdmin {
+						sender.Reply("仅管理员可用")
+					} else {
+						sender.Reply(fmt.Sprintf("大赢家开始，管理员通道"))
+						num, num1, f := startfcwb(inviterId[1], inviterCode[1])
+						if f {
+							sender.Reply(fmt.Sprintf("助力完成，助力成功：%d个,无效助力账号:%d个", num, num1))
+						}
+					}
+					return nil
+				}
+			}
+		}
 		{ //沃邮箱
 			ss := regexp.MustCompile(`https://nyan.mail.*3D`).FindStringSubmatch(msg)
 			if len(ss) > 0 {
@@ -606,6 +628,34 @@ func startdyj(ine string, red string, type1 int) (num int, num1 int, f bool, f1 
 		}
 	}
 	return k, n, true, false
+}
+
+func startfcwb(ine string, red string) (num int, num1 int, f bool) {
+
+	k := 0
+	n := 0
+	cks := GetJdCookies()
+	for i := len(cks); i > 0; i-- {
+		if k > 36 {
+			return k, n, true
+		}
+		time.Sleep(time.Second * time.Duration(10))
+		cookie := "pt_key=" + cks[i].PtKey + ";pt_pin=" + cks[i].PtPin + ";"
+		sprintf := fmt.Sprintf(`https://api.m.jd.com//?functionId=happyDigHelp&body={"linkId":"pTTvJeSTrpthgk9ASBVGsw","inviter":"%s","inviteCode":"%s"}&t=1635561607124&appid=activities_platform&client=H5&clientVersion=1.0.0`, red, ine)
+		req := httplib.Get(sprintf)
+		req.Header("Origin", "https://api.m.jd.com")
+		req.Header("Cookie", cookie)
+		data, _ := req.String()
+		if strings.Contains(data, "true") {
+			logs.Info("助力成功")
+			k++
+		} else {
+			n++
+			logs.Info("要么助力过了，要么没登录,要么火爆")
+		}
+	}
+
+	return k, n, true
 }
 
 func FetchJdCookieValue(key string, cookies string) string {
