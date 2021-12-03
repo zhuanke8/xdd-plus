6f9ebcd6d17ae1fa394c7a684731c6525f0b6ddd
ZJY <764763903@qq.com>
2021-12-03 21:31:34 +0800

    修复重大BUG 定时检测写错位置导致没有检测妈的智障操作。

--- models/cron.go
+++ models/cron.go
@@ -21,7 +21,7 @@ func initCron() {
 		c.AddFunc("3 */1 * * *", initVersion)
 		c.AddFunc("40 */1 * * *", GitPullAll)
 	}
-	logs.Info("0 " + strconv.Itoa(rand.Intn(59)) + " 0/" + strconv.Itoa(Config.Later) + " * * ?" + "调试推送时间")
+	//logs.Info("0 " + strconv.Itoa(rand.Intn(59)) + " 0/" + strconv.Itoa(Config.Later) + " * * ?" + "调试推送时间")
 	c.AddFunc("0 "+strconv.Itoa(rand.Intn(59))+" 0/"+strconv.Itoa(Config.Later)+" * * ?", initCookie)
 
 	c.Start()
