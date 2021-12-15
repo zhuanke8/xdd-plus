78b14552a6d1cf79a320fb81b7f9f9e038b209ab
ZJY <764763903@qq.com>
2021-12-15 18:14:50 +0800

    保存代码

--- models/handle.go
+++ models/handle.go
@@ -26,6 +26,7 @@ func initHandle() {
 			cks := GetJdCookies(func(sb *gorm.DB) *gorm.DB {
 				return sb.Where(fmt.Sprintf("%s >= ? and %s != ?", Priority, Hack), 0, True)
 			})
+			logs.Info(fmt.Sprintf("总共%d个号", len(cks)))
 			tmp := []JdCookie{}
 			for _, ck := range cks {
 				if ck.Priority >= 0 && ck.Hack != True {
