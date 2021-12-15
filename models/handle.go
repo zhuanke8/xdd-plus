1539e7816fadd8e71b15cccb806a99544b62ec44
ZJY <764763903@qq.com>
2021-12-15 23:38:30 +0800

    新增超时转换提醒

--- models/handle.go
+++ models/handle.go
@@ -23,7 +23,7 @@ func initHandle() {
 				continue
 			}
 			cks := GetJdCookies(func(sb *gorm.DB) *gorm.DB {
-				return sb.Where(fmt.Sprintf("%s >= ? and %s != ?", Priority, Hack), 0, True)
+				return sb.Where(fmt.Sprintf("%s >= ? and %s != ? and %s = ?", Priority, Hack, Available), 0, True, True)
 			})
 			logs.Info(fmt.Sprintf("总共%d个号", len(cks)))
 			tmp := []JdCookie{}
