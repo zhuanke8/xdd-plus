88517a9d00b6fecf81c622fcde745e28012042d7
ZJY <764763903@qq.com>
2021-12-15 23:39:00 +0800

    修复过期数量加入平衡问题，感谢来自大白熊的PR

--- models/handle.go
+++ models/handle.go
@@ -25,6 +25,7 @@ func initHandle() {
 			cks := GetJdCookies(func(sb *gorm.DB) *gorm.DB {
 				return sb.Where(fmt.Sprintf("%s >= ? and %s != ? and %s = ?", Priority, Hack, Available), 0, True, True)
 			})
+
 			logs.Info(fmt.Sprintf("总共%d个号", len(cks)))
 			tmp := []JdCookie{}
 			for _, ck := range cks {
