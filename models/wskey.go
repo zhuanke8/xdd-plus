ac58f56793865236a9a8ca992f38b2b8ed0b7edc
ZJY <764763903@qq.com>
2022-01-11 22:24:25 +0800

    修改为8小时更新一次WSKEY账号

--- models/wskey.go
+++ models/wskey.go
@@ -12,7 +12,7 @@ func intiSky() {
 	c := cron.New(cron.WithSeconds()) //精确到秒
 
 	//定时任务
-	spec := "0 " + strconv.Itoa(rand.Intn(59)) + " " + Config.CTime + "/12 * * ?" //cron表达式，每秒一次
+	spec := "0 " + strconv.Itoa(rand.Intn(59)) + " " + Config.CTime + "/8 * * ?" //cron表达式，每秒一次
 	logs.Info(spec)
 	if Config.Wskey {
 		c.AddFunc(spec, func() {
