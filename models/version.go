cd70ba7c74e47456374fb316f0ddef0183aae156
ZJY <764763903@qq.com>
2022-01-11 23:16:34 +0800

    修改版本号

--- models/version.go
+++ models/version.go
@@ -9,7 +9,7 @@ import (
 	"strings"
 )
 
-var version = "v2.0"
+var version = "v2.2"
 var describe = "最终稳定版"
 var AppName = "xdd"
 var pname = regexp.MustCompile(`/([^/\s]+)`).FindStringSubmatch(os.Args[0])[1]
