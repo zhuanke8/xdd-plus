453d2d33406318e63b0ba424136648fed4f9f10f
ZJY <764763903@qq.com>
2022-01-29 01:40:36 +0800

    修复登录问题

--- models/cache.go
+++ models/cache.go
@@ -1,6 +1,6 @@
 package models
 
-import "time"
+import "github.com/beego/beego/v2/core/logs"
 
 type Cache struct {
 	key      string `gorm:"column:Key;primaryKey"`
@@ -11,8 +11,8 @@ type Cache struct {
 
 func getCache(key string) (value string) {
 	u := &Cache{}
-	format := "2006-01-02 15:04:05"
-	err := db.Where("key = ? and active_at < ?", key, time.Now().Format(format)).First(&u).Error
+	//format := "2006-01-02 15:04:05"
+	err := db.Where("key = ? and active_at < ?", key, Date()).First(&u).Error
 	if err == nil {
 		return u.value
 	} else {
@@ -20,23 +20,26 @@ func getCache(key string) (value string) {
 	}
 }
 
-//func saveCache(cache Cache) (flag bool) {
-//	u := &Cache{}
-//	err := db.Where("key = ? and active_at < ?", key, time.Now()).First(&u).Error
-//	if err == nil {
-//		if u.ActiveAt < time.Now() {
-//			db.Where("ID = ?", u.ID).Updates(&Limit{
-//				Num: u.Num + 1,
-//			})
-//			return true
-//		} else {
-//			return false
-//		}
-//	} else {
-//		return u.value
-//	}
-//	begin := db.Begin()
-//	begin.Create(cache)
-//	commit := begin.Commit()
-//
-//}
+func saveCache(key string, value string) (flag bool) {
+	u := &Cache{}
+	err := db.Where("key = ? and active_at = ?", key, Date()).First(&u).Error
+	if err == nil {
+		logs.Info("为空不报错")
+		if u.value != "" {
+			db.Where("ID = ?", u.key).Updates(&Cache{
+				value: value,
+			})
+			return true
+		} else {
+			u.key = key
+			u.value = value
+			begin := db.Begin()
+			begin.Create(u)
+			begin.Commit()
+			return true
+		}
+	} else {
+		return false
+	}
+
+}
