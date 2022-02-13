a2f1225f99a5a43c9377cb628094214f488d6d61
ZJY <764763903@qq.com>
2022-02-13 23:53:51 +0800

    修复诺兰登录

--- models/bot.go
+++ models/bot.go
@@ -758,16 +758,16 @@ var handleMessage = func(msgs ...interface{}) interface{} {
 					sender.Reply(fmt.Sprintf("推一推即将开始，已扣除%d个互助值，管理员通道", Config.Tyt))
 				}
 
-				//runTask(&Task{Path: "jd_tyt.js", Envs: []Env{
-				//	{Name: "tytpacketId", Value: ss[1]},
-				//}}, sender)
-				num, f := starttyt(ss[1])
-				if f {
-					return fmt.Sprintf("推一推结束共用:%d个账号", num)
-				} else {
-					return "推一推失败"
-				}
-				return "推一推已结束"
+				runTask(&Task{Path: "jd_tyt.js", Envs: []Env{
+					{Name: "tytpacketId", Value: ss[1]},
+				}}, sender)
+				//num, f := starttyt(ss[1])
+				//if f {
+				//	return fmt.Sprintf("推一推结束共用:%d个账号", num)
+				//} else {
+				//	return "推一推失败"
+				//}
+				//return "推一推已结束"
 			}
 		}
 		{
