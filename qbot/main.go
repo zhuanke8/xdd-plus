1a33cd2a8812ded78d59481cc61c3cb0309a39b9
ZJY <764763903@qq.com>
2022-03-08 02:42:27 +0800

    大版本修复

--- qbot/main.go
+++ qbot/main.go
@@ -75,8 +75,8 @@ func Main() {
 				if strings.Contains(msg.(string), "data:image") {
 					//bot.UploadLocalImageAsPrivate(uid, &coolq.LocalImageElement{File: "./output.jpg"})
 					log.Info("开始发送图片")
-					bot.SendPrivateMessage(uid, models.Config.QQGroupID, &message.SendingMessage{Elements: []message.IMessageElement{&coolq.LocalImageElement{File: "./output.jpg"}}})
-
+					bot.SendPrivateMessage(uid, models.Config.QQGroupID, &message.SendingMessage{Elements: []message.IMessageElement{&coolq.LocalImageElement{File: "output.jpg"}}})
+					bot.UploadLocalImageAsPrivate(uid, &coolq.LocalImageElement{})
 				} else {
 					bot.SendPrivateMessage(uid, models.Config.QQGroupID, &message.SendingMessage{Elements: []message.IMessageElement{&message.TextElement{Content: msg.(string)}}})
 				}
