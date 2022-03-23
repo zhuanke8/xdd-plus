d277d424d99c77ee8c5fbda0b63ba78823702a06
ZJY <764763903@qq.com>
2022-03-23 14:17:41 +0800

    更改动物与接口

--- models/wck.go
+++ models/wck.go
@@ -131,7 +131,7 @@ func getKey(WSCK string) (string, error) {
 */
 
 func getToken() string {
-	data, _ := httplib.Post("https://api.jds.codes/gentoken").SetUserAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.54 Safari/537.36").Bytes()
+	data, _ := httplib.Post("https://api.jds.codes/genToken").SetUserAgent("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.54 Safari/537.36").Bytes()
 	getString, _ := jsonparser.GetString(data, "data", "sign")
 	return getString
 }
