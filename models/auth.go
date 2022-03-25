9f5a4d9825b983d13611b09607af8f31ff11515e
ZJY <764763903@qq.com>
2022-03-25 17:55:00 +0800

    json

--- models/auth.go
+++ models/auth.go
@@ -21,7 +21,6 @@ func GetAuthKey() {
 	post := httplib.Post("http://auth.smxy.xyz/user/auth1")
 	post.Param("qqNum", strconv.FormatInt(Config.QQID, 10))
 	post.Param("master", Config.Master)
-	log.Info(Config.Master)
 	post.Param("uid", strconv.FormatInt(Config.QQGroupID, 10))
 	post.Bytes()
 }
@@ -30,6 +29,6 @@ func getAuthFlag() {
 	post := httplib.Post("http://auth.smxy.xyz/user/authFlag")
 	post.Param("qqNum", strconv.FormatInt(Config.QQID, 10))
 	s, _ := post.Bytes()
-	getString, _ := jsonparser.GetString(s, "data")
+	getString, _ := jsonparser.GetBoolean(s, "data")
 	log.Info(getString)
 }
