97237297fd7aa6aec95060637dd960538474656f
ZJY <764763903@qq.com>
2022-03-29 23:58:37 +0800

    fanli 内置推一推

--- models/bot.go
+++ models/bot.go
@@ -902,7 +902,7 @@ func starttyt(red string) (num int, f bool) {
 	db.Where(fmt.Sprintf("%s != 'false' and %s = 'true'", Tyt, Available)).Order("RAND()").Find(&cks)
 	logs.Info(len(cks))
 	for _, ck := range cks {
-		time.Sleep(time.Second * 5)
+		time.Sleep(time.Second * 7)
 		logs.Info(ck.PtPin)
 		cookie := "pt_key=" + ck.PtKey + ";pt_pin=" + ck.PtPin + ";"
 		sprintf := fmt.Sprintf(`https://api.m.jd.com/?functionId=helpCoinDozer&appid=station-soa-h5&client=H5&clientVersion=1.0.0&t=1641900500241&body={"actId":"49f40d2f40b3470e8d6c39aa4866c7ff","channel":"coin_dozer","referer":"-1","frontendInitStatus":"s","packetId":"%s","helperStatus":"0"}&_ste=1`, red)
